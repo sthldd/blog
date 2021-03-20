2a39c9f144def1d3900ac51d303ef69fc05ed721
maliang <maliang@ixiye.com>
2021-03-20 16:28:11 +0800

    sh脚本

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,8 @@
-echo hello > /tmp/frank
+docker start 015 &&
+cd /home/blog/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build -t mlx/node-web-app . &&
+docker run --name app --network=host  -p 3000:3000 -d mlx/node-web-app &&
+echo 'ok!'
