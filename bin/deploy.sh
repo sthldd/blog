1d3c0ff63b6bd9c6ea3fe30717718a79baeb155b
马理想 <1209624528@qq.com>
2022-05-02 23:37:16 +0800

    配置

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,10 @@
 docker start distracted_buck &&
 cd /home/blog/app/blog &&
 git pull &&
-yarn install --production=false &&
+npm install --production=false &&
 yarn build &&
 docker build -t mlx/node-web-app . &&
-docker kill app && 
-docker remove app && 
-docker run --name app --network=host  -p 3000:3000 -d mlx/node-web-app &&
+docker kill distracted_buck && 
+docker remove distracted_buck && 
+docker run  --network=host  -p 3000:3000 -d mlx/node-web-app &&
 echo 'ok!' 
